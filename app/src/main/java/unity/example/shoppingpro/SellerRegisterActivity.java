c0b9f08bc5ef7fa919a5a0ad597494dd52381273
Kavindu Ushan <kavinduushan07@gmail.com>
2021-10-04 21:36:29 +0530

    Initial commit

---
+++ app/src/main/java/unity/example/shoppingpro/SellerRegisterActivity.java
@@ -0,0 +1,14 @@
+package unity.example.shoppingpro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SellerRegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seller_register);
+    }
+}
